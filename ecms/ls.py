8f36c4ff33d60625a1233f42af4ae9c01dfd139f
david <david.m.m.antunes@gmail.com>
2020-09-15 13:39:48 +0100

    change example to ls and added color to different type of files

--- ecms/ls.py
+++ ecms/ls.py
@@ -1,12 +1,17 @@
 
 import os
-
+from colorama import Fore
 def main():
     print('This is all of the available scripts.')
     print('Type the name of the file and any necessary arguments.')
     p = os.listdir()
     for file in p:
-        print(file)
+        if(file[0] == '.'):
+            print(Fore.RED + file)
+        elif os.path.isdir(file):
+            print(Fore.BLUE + file)
+        else:
+            print(Fore.GREEN + file)
 
 
 if __name__ == "__main__":
